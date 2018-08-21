6b775b704321ff86e153443b89a868d5af8b7d6f
gitliyu <1042625709@qq.com>
2018-08-21 18:21:37 +0800

    change to es5

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 export default {
   /*
-  * params
-  * Vue: Vue实例
-  * Router: Router实例
-  * title: 未设置时默认的title值
-  * */
+   * params
+   * Vue: Vue实例
+   * Router: Router实例
+   * title: 未设置时默认的title值
+   * */
   install: function (Vue, Router, title) {
-    Router.beforeEach((to, from, next) => {
+    Router.beforeEach(function(to, from, next) {
       this.setTitle(to.meta.title, title);
       next()
     });
@@ -16,20 +16,20 @@ export default {
    * title: 设置的title属性
    * defaultTitle: 未设置时默认的title值
    * */
-  setTitle: (title, defaultTitle) => {
-    let pageTitle = title || defaultTitle;
+  setTitle: function (title, defaultTitle) {
+    var pageTitle = title || defaultTitle;
     // 直接修改文档title
     document.title = pageTitle;
     // ios的微信端，使用iframe修改title
-    let userAgent = window.navigator.userAgent.toLowerCase();
-    let isiOS = userAgent.indexOf('applewebkit') >= 0;
-    let isWechat = userAgent.indexOf('micromessenger') >= 0;
+    var userAgent = window.navigator.userAgent.toLowerCase();
+    var isiOS = userAgent.indexOf('applewebkit') >= 0;
+    var isWechat = userAgent.indexOf('micromessenger') >= 0;
     if (isiOS && isWechat) {
-      let iframe = document.createElement('iframe');
+      var iframe = document.createElement('iframe');
       iframe.style.display = 'none';
       document.body.appendChild(iframe);
-      iframe.onload = () => {
-        setTimeout(() => {
+      iframe.onload = function () {
+        setTimeout(function(){
           iframe.remove();
         }, 0)
       };
